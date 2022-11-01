c1162e6d432aebaa80b759b6a2cfbe6778a3764c
Binwakil <almustaphawakili@gmail.com>
2022-11-01 10:37:26 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {Navbar,Footer} from './components'
+import {Home,Profile,Item, Create,Login,Register} from './pages'
+import { Routes, Route } from "react-router-dom"
+import * as React from 'react';
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path=":item/:id" element={<Item />} />
+            <Route path="/create" element={<Create /> } />
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/login" element={ <Login />} />
+            <Route path="/register" element={ <Register />} />
+          </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
